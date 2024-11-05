d614e0ed7ba9463b9baecbebe3de76748f80b326
GordelaG <ailtonplay2700@gmail.com>
2024-11-05 07:12:42 -0300

    Remoção dos shells do ps-housing
    Removido linhas que chamavam mapas da pasta stream.
    Fix for "stuck in black loading screen será adicionado na noma pasta de shells para evitar crash quando reiniciar o ps-housing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -40,19 +40,4 @@ server_script {
 
 files {
   'html/**',
-  'stream/starter_shells_k4mb1.ytyp',
-  'stream/**.ytyp'
 }
-
--- Fix for "stuck in black loading screen"
-data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_corporate.rpf/int_corporate.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_industrial.rpf/int_industrial.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_lev_des.rpf/int_lev_des.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_residential.rpf/int_residential.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_retail.rpf/int_retail.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_services.rpf/int_services.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'starter_shells_k4mb1.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/**.ytyp'
-
-this_is_a_map 'yes'
